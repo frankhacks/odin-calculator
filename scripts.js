2b6782e97a46a565e16caf477a4dad62f34b8e46
Frank Huang <frankluxinghuang@gmail.com>
2023-01-31 22:41:06 -0500

    Basic calc function without clear

---
+++ scripts.js
@@ -0,0 +1,184 @@
+var answer = 0;
+var x = 0;
+var y = 0;
+let operator = 0;
+
+let add = function (a, b) {
+    answer = parseInt(a) + parseInt(b);
+    return answer;
+};
+
+let subtract = function (a, b) {
+    answer = a - b;
+    return answer;
+};
+
+let multiply = function (a, b) {
+    answer = a * b;
+    return answer;
+};
+
+let divide = function (a, b) {
+    answer = a / b;
+    return answer;
+};
+
+let operate = function (operator, a, b) {
+    if (operator === "m") {
+        multiply(a,b);
+    } else if (operator === "d") {
+        divide(a,b);
+    } else if (operator === "s") {
+        subtract(a,b);
+    } else if (operator === "a") {
+        add(a,b);
+    }
+};
+
+let inputs = document.getElementById('results');
+
+document.getElementById('1').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "1";
+    }
+    else {
+    inputs.innerHTML += "1";
+    }
+};
+
+document.getElementById('2').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "2";
+    }
+    else {
+    inputs.innerHTML += "2";
+    }
+};
+
+document.getElementById('3').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "3";
+    }
+    else {
+    inputs.innerHTML += "3";
+    }
+};
+
+document.getElementById('4').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "4";
+    }
+    else {
+    inputs.innerHTML += "4";
+    }
+};
+
+document.getElementById('5').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "5";
+    }
+    else {
+    inputs.innerHTML += "5";
+    }
+};
+
+document.getElementById('6').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "6";
+    }
+    else {
+    inputs.innerHTML += "6";
+    }
+};
+
+document.getElementById('7').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "7";
+    }
+    else {
+    inputs.innerHTML += "7";
+    }
+};
+
+document.getElementById('8').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "8";
+    }
+    else {
+    inputs.innerHTML += "8";
+    }
+};
+
+document.getElementById('9').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "9";
+    }
+    else {
+    inputs.innerHTML += "9";
+    }
+};
+
+document.getElementById('0').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += "0";
+    }
+    else {
+    inputs.innerHTML += "0";
+    }
+};
+
+document.getElementById('decimal').onclick = function() {
+    if (parseInt(inputs.innerHTML) === 0){
+        inputs.innerHTML = "";
+        inputs.innerHTML += ".";
+    }
+    else if (inputs.innerHTML.includes(".")) {
+    }
+    else {
+    inputs.innerHTML += ".";
+    }
+};
+
+document.getElementById('add').onclick = function() {
+    x = inputs.innerHTML;
+    operator = "a";
+    inputs.innerHTML="0";
+    console.log(x);
+}
+
+document.getElementById('subtract').onclick = function() {
+    x = inputs.innerHTML;
+    operator = "s";
+    inputs.innerHTML="0";
+    console.log(x);
+}
+
+document.getElementById('multiply').onclick = function() {
+    x = inputs.innerHTML;
+    operator = "m";
+    inputs.innerHTML="0";
+    console.log(x);
+}
+
+document.getElementById('divide').onclick = function() {
+    x = inputs.innerHTML;
+    operator = "d";
+    inputs.innerHTML="0";
+    console.log(x);
+}
+
+document.getElementById('equal').onclick = function() {
+    y = inputs.innerHTML;
+    operate(operator, x, y);
+    inputs.innerHTML = answer;
+}
